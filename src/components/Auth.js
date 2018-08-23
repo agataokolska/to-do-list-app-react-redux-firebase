ad13c4a2e3b471bc166408092941a3f9bad3dc02
agata okolska <agata.okolska@gmail.com>
2018-08-23 14:19:58 +0200

    add login by google

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,45 +1,57 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import RaisedButton from 'material-ui/RaisedButton'
-import {onEmailChangeAction,
+import {
+    onEmailChangeAction,
     onPasswordChangeAction,
     onLoginClickAction,
-    onLogOutAction} from '../state/auth'
+    onLogOutAction
+} from '../state/auth'
 import LoginByEmailAndPassword from './LoginByEmailAndPassword'
+import { onLogInByGoogleClickHandler } from '../state/logInGoogleAuth'
 
 const Auth = (props) => (
     props._user ?
-    <div>
-    <RaisedButton onClick={props._onLogOutAction}
-    >
-    LOG OUT
+        <div>
+            <RaisedButton onClick={props._onLogOutAction}
+            >
+                LOG OUT
     </RaisedButton>
-    {props.children}
-    </div>
-     :
-    <div>
-        <LoginByEmailAndPassword
-        emailValue={props._emailValue}
-        onEmailChange={props._onEmailChange}
-        passwordValue={props._passwordValue}
-        onPasswordChange={props._onPasswordChange}
-        onLoginClick={props._onLoginClick}
+            {props.children}
+        </div>
+        :
+        <div>
+            <LoginByEmailAndPassword
+                emailValue={props._emailValue}
+                onEmailChange={props._onEmailChange}
+                passwordValue={props._passwordValue}
+                onPasswordChange={props._onPasswordChange}
+                onLoginClick={props._onLoginClick}
+            />
+            <RaisedButton
+                label="Login by google"
+                primary={true}
+                onClick={props._onLogInByGoogleClickHandler}
+            />
+        </div>
 
-        />
-    </div>
 )
 const mapStateToProps = state => ({
-_emailValue: state.auth.email,
-_passwordValue: state.auth.password,
-_user: state.auth.user
+    _emailValue: state.auth.email,
+    _passwordValue: state.auth.password,
+    _user: state.auth.user
 })
 
 const mapDispatchToProps = dispatch => ({
-_onEmailChange: event => dispatch(onEmailChangeAction(event.target.value)),
-_onPasswordChange: event => dispatch(onPasswordChangeAction(event.target.value)),
-_onLoginClick: () => dispatch(onLoginClickAction()),
-_onLogOutAction: () => dispatch(onLogOutAction())
+    _onEmailChange: event => dispatch(onEmailChangeAction(event.target.value)),
+    _onPasswordChange: event => dispatch(onPasswordChangeAction(event.target.value)),
+    _onLoginClick: () => dispatch(onLoginClickAction()),
+    _onLogOutAction: () => dispatch(onLogOutAction()),
+
+    _onLogInByGoogleClickHandler: () => dispatch(onLogInByGoogleClickHandler()),
 })
+
+
 
 export default connect(mapStateToProps,
     mapDispatchToProps)(Auth)
---
+++ src/state/logInGoogleAuth.js
@@ -0,0 +1,9 @@
+import { auth as firebaseAuth, googleProvider } from '../firebaseConfig'
+
+export const onLogInByGoogleClickHandler = () => () => {
+    firebaseAuth.signInWithPopup(googleProvider)
+        .catch((error) => {
+            console.log(error)
+            alert('Błąd logowania')
+        })
+}
